5240301d307e60586f541ca205325844ef512eb6
João Neffa <jpsousa837@gmail.com>
2023-05-21 21:56:34 -0300

    Css basico nav.html

--- apps/bolsas/urls.py
+++ apps/bolsas/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from apps.bolsas.views import \
-    index
+    index, locations, historico, login
 
 urlpatterns = [
     path('', index, name='index'),
+    path('locations/', locations, name='locations'),
+    path('historico/', historico, name='historico'),
+    path('login/', login, name='login'),
 ]
--- apps/bolsas/views.py
+++ apps/bolsas/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'bolsas/index.html')
+
+def locations(request):
+    return render(request, 'bolsas/locations.html')
+
+def historico(request):
+    return render(request, 'historico.html')
+
+def login(request):
+    return render(request, 'login.html')
